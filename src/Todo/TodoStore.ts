dbd5fe50f2bb3c80059a13baf4a9f48e86d54f0d
macstojak <maciej.stojak89@wp.pl>
2021-08-10 13:04:43 +0200

    working todos

--- src/Todo/TodoStore.ts
+++ src/Todo/TodoStore.ts
@@ -1,58 +1,47 @@
-import { makeObservable, action, observable } from "mobx";
+import { makeAutoObservable, action, observable } from "mobx";
 import { createContext } from "react";
 import Todo from "./TodoModel";
 
-class TodoStore {
-    todos: Todo[] = observable([
-        { id: "papdowapopd21)@@_", completed: false, title: "pet dog" }
-    ]);
+const TodoStore = () => {
+    return makeAutoObservable({
+        todos: [
+            { id: "papdowapopd21)@@_", completed: false, title: "pet dog" }
+        ],
 
-    constructor() {
-        makeObservable(this, {
-            todos: observable,
-            getTodos: action,
-            addTodo: action,
-            toggleTodo: action,
-            editTodo: action,
+        getTodos() {
+            return this.todos;
+        },
+
+        addTodo(todo: Todo) {
+            console.log("todo", todo)
+            this.todos.push(todo);
+        },
+
+        changeBool(result: boolean) {
+            return !result;
+        },
+
+        toggleTodo(id: string) {
+            this.todos.filter(el => {
+                if (el.id === id) {
+                    el.completed = this.changeBool(el.completed);
+                }
+                return el.completed;
+            })
+        },
+
+        editTodo(todo: Todo) {
+            this.todos.filter(el => {
+                if (todo.id === el.id) {
+                    return el.title = todo.title;
+                }
+                return el;
+            })
+        },
+
+        deleteTodo(id: string) {
+            this.todos.splice(this.todos.findIndex(el => el.id === id), 1);
         }
-        )
-    }
-
-    getTodos() {
-        return this.todos;
-    }
-
-    addTodo(todo: Todo) {
-        console.log("todo", todo)
-        return this.todos.push(todo);
-    };
-
-    changeBool(result: boolean) {
-        return !result;
-    }
-
-    toggleTodo(id: string) {
-        console.log("TOGLE")
-        this.todos.filter(el => {
-            if (el.id === id) {
-                el.completed = this.changeBool(el.completed);
-            }
-            return el.completed;
-        })
-    }
-
-    editTodo(todo: Todo) {
-        this.todos.filter(el => {
-            if (todo.id === el.id) {
-                return el.title = todo.title;
-            }
-            return el;
-        })
-    }
-
-    deleteTodo(id: string) {
-        this.todos.splice(this.todos.findIndex(el => el.id === id), 1);
-    }
-
+    })
 }
-export const TodoStoreContext = createContext(new TodoStore());
+export const TodoStoreContext = createContext(TodoStore());
